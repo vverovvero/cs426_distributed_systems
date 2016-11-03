55f684a17bcbe23c4b4dce41e77fe6bd14bb80bc
vverovvero <wendy.w.chen@yale.edu>
2016-11-03 01:39:26 -0400

    debugging testing for new checkpoint function

--- checkpoint.h
+++ checkpoint.h
@@ -1,55 +1,47 @@
-// //checkpoint.h 										Wendy Chen, 10/2016
+//checkpoint.h 										Wendy Chen, 10/2016
 
 
-// #include "graph.h"
+#include "graph.h"
 
-// #include "stdint.h" //has type 'uint64_t'
+#include "stdint.h" //has type 'uint64_t'
 
-// #include <sys/mman.h>
-// #include <sys/stat.h>
-// #include <fcntl.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <sys/types.h>
-
-
-// #include <assert.h>
-// #include <inttypes.h>
-
-// #include <iostream>
-// #include <stdlib.h> //has exit, EXIT_FAILURE
-// // #include <cstdint> //has type 'uint64_t'
-// #include <utility> //has type 'pair'
-// #include <map>
-// #include <set>
-// #include <algorithm>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 
-// using std::pair; 
-// using std::map;
-// using std::set;
+#include <assert.h>
+#include <inttypes.h>
 
-// #ifndef CHECKPOINT_H
-// #define CHECKPOINT_H
+#include <iostream>
+#include <stdlib.h> //has exit, EXIT_FAILURE
+// #include <cstdint> //has type 'uint64_t'
+#include <utility> //has type 'pair'
+#include <map>
+#include <set>
+#include <algorithm>
 
 
-// void * ch_load_block();
-// void ch_free_block(void * addr);
-// // void ch_synchronize(void *dest, const void *source);
-// uint64_t ch_set_checksum(void * addr);
-// int ch_write_checkpoint(void * addr, Graph *graph);
+using std::pair; 
+using std::map;
+using std::set;
 
-// void ch_write_disk(int fd, const void *addr);
-// void ch_read_disk(int fd, void *addr);
+#ifndef CHECKPOINT_H
+#define CHECKPOINT_H
 
-// int dump_checkpoint(int fd, Graph *graph);
+void * ch_load_block(size_t length);
+void ch_free_block(void * addr, size_t length);
+uint64_t ch_set_checksum(void * addr, size_t length);
+int ch_get_serial_size(Graph *graph);
+void ch_write_checkpoint(uint64_t *checkpoint, int serial_size_bytes, Graph *graph);
+void ch_write_superblock(Ch_Superblock *ch_superblock, uint32_t generation, uint32_t num_blocks);
 
-// int load_checkpoint(int fd, Graph *graph);
+int dump_checkpoint(int fd, Graph *graph, uint32_t generation);
 
-// int print_checkpoint(int fd);
 
-// int ch_check_validity_checkpoint(int fd);
-
-// #endif
+#endif
 
