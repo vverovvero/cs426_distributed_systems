adc33203df693fd82fcb9ed5206a9000fdb9fe06
vverovvero <wendy.w.chen@yale.edu>
2016-11-05 16:07:26 -0400

    Change mytest.sh to be 500

--- mytest.sh
+++ mytest.sh
@@ -2,7 +2,7 @@
 
 echo "Beginning test script"
 
-for i in {1..204}
+for i in {1..500}
 do
 	curl -H "Content-Type: application/json" -X POST -d '{"node_id":'$i'}' http://127.0.0.1:8000/api/v1/add_node
 done
