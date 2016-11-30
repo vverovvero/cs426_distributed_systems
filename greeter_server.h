fbaf433f30e103168fe810456d1e5e22a0825438
vverovvero <wendy.w.chen@yale.edu>
2016-11-29 21:32:00 -0500

    trying something for compiling; .h files?

--- greeter_server.h
+++ greeter_server.h
@@ -30,7 +30,7 @@ class GreeterServiceImpl final : public Greeter::Service {
     reply->set_message(prefix + request->name());
     return Status::OK;
   }
-  
+
   Status SayHelloAgain(ServerContext *context, const HelloRequest* request,
 		  HelloReply* reply) override {
     std::string prefix("Hello again ");
@@ -39,25 +39,7 @@ class GreeterServiceImpl final : public Greeter::Service {
   }
 };
 
-void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  GreeterServiceImpl service;
-
-  ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
-
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
-}
-
+void RunServer(void);
 
 // int main(int argc, char** argv);
 
