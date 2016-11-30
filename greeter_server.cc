eb89eccb9ccdea7279a29f463e791d8169672750
vverovvero <wendy.w.chen@yale.edu>
2016-11-29 21:36:00 -0500

    trying something for compiling; can UnServer in cs426_graph_server?

--- greeter_server.cc
+++ greeter_server.cc
@@ -49,24 +49,24 @@ using helloworld::HelloReply;
 using helloworld::Greeter;
 
 
-void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  GreeterServiceImpl service;
+// void RunServer() {
+//   std::string server_address("0.0.0.0:50051");
+//   GreeterServiceImpl service;
 
-  ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+//   ServerBuilder builder;
+//   // Listen on the given address without any authentication mechanism.
+//   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+//   // Register "service" as the instance through which we'll communicate with
+//   // clients. In this case it corresponds to an *synchronous* service.
+//   builder.RegisterService(&service);
+//   // Finally assemble the server.
+//   std::unique_ptr<Server> server(builder.BuildAndStart());
+//   std::cout << "Server listening on " << server_address << std::endl;
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
-}
+//   // Wait for the server to shutdown. Note that some other thread must be
+//   // responsible for shutting down the server for this call to ever return.
+//   server->Wait();
+// }
 
 int main(int argc, char** argv) {
   RunServer();
